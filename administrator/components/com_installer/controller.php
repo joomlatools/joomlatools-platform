76cfc01157a47fb8541bad29f82429ba952fb402
Johan Janssens <johan@janssens.me>
2015-05-28 01:28:25 +0200

    #1 - Remove the extension installer user interface. Only leave the extension manager in place to allow enabling and disabling extensions easily.

--- administrator/components/com_installer/controller.php
+++ administrator/components/com_installer/controller.php
@@ -36,7 +36,7 @@ class InstallerController extends JControllerLegacy
 		$document = JFactory::getDocument();
 
 		// Set the default view name and format from the Request.
-		$vName   = $this->input->get('view', 'install');
+		$vName   = $this->input->get('view', 'manage');
 		$vFormat = $document->getType();
 		$lName   = $this->input->get('layout', 'default', 'string');
 
