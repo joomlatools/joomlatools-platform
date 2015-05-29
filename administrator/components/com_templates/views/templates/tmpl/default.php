4ca4a11fc460103707396eafffd231fc39b2c42f
Johan Janssens <johan@janssens.me>
2015-05-29 19:57:47 +0200

    #102 - Remove the 'template' view and template edit functionality.

--- administrator/components/com_templates/views/templates/tmpl/default.php
+++ administrator/components/com_templates/views/templates/tmpl/default.php
@@ -75,8 +75,7 @@ $listDirn	= $this->escape($this->state->get('list.direction'));
 					<?php echo JHtml::_('templates.thumb', $item->element, $item->client_id); ?>
 				</td>
 				<td class="template-name">
-					<a href="<?php echo JRoute::_('index.php?option=com_templates&view=template&id=' . (int) $item->extension_id . '&file=' . $this->file); ?>">
-						<?php echo JText::sprintf('COM_TEMPLATES_TEMPLATE_DETAILS', ucfirst($item->name)); ?></a>
+					<strong><?php echo JText::sprintf('COM_TEMPLATES_TEMPLATE_DETAILS', ucfirst($item->name)); ?></strong>
 					<p>
 					<?php if ($this->preview && $item->client_id == '0') : ?>
 						<a href="<?php echo JUri::root() . 'index.php?tp=1&template=' . $item->element; ?>" target="_blank">
