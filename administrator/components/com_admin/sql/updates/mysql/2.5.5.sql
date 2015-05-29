abe984753a24d4776bf4478999eeb1da1aac54a2
Johan Janssens <johan@janssens.me>
2015-05-29 14:58:12 +0200

    #108 - Remove updates SQL

--- administrator/components/com_admin/sql/updates/mysql/2.5.5.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `#__users` ADD COLUMN `lastResetTime` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'Date of last password reset';
-ALTER TABLE `#__users` ADD COLUMN `resetCount` int(11) NOT NULL DEFAULT '0' COMMENT 'Count of password resets since lastResetTime';
