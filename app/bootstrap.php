8718062808b043651fbe4c92adc7dd741567d94f
Johan Janssens <johan@janssens.me>
2015-06-13 22:20:57 +0200

    #125 - Remove error_reporting config setting

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -37,44 +37,6 @@ ob_end_clean();
 // System configuration.
 $config = new JConfig;
 
-// Set the error_reporting
-switch ($config->error_reporting)
-{
-	case 'default':
-	case '-1':
-		break;
-
-	case 'none':
-	case '0':
-		error_reporting(0);
-
-		break;
-
-	case 'simple':
-		error_reporting(E_ERROR | E_WARNING | E_PARSE);
-		ini_set('display_errors', 1);
-
-		break;
-
-	case 'maximum':
-		error_reporting(E_ALL);
-		ini_set('display_errors', 1);
-
-		break;
-
-	case 'development':
-		error_reporting(-1);
-		ini_set('display_errors', 1);
-
-		break;
-
-	default:
-		error_reporting($config->error_reporting);
-		ini_set('display_errors', 1);
-
-		break;
-}
-
 define('JDEBUG', $config->debug);
 
 // System profiler
