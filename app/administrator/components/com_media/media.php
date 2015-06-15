7aad51541c390f0c0deba2b6673ca753b0c8b6c6
Johan Janssens <johan@janssens.me>
2015-06-15 23:58:51 +0200

    #111 - Use JPATH_WEB instead of JPATH_ROOT

--- app/administrator/components/com_media/media.php
+++ app/administrator/components/com_media/media.php
@@ -40,7 +40,7 @@ if (substr(strtolower($view), 0, 6) == 'images' || $popup_upload == 1)
 	$path = 'image_path';
 }
 
-define('COM_MEDIA_BASE',    JPATH_ROOT . '/' . $params->get($path, 'images'));
+define('COM_MEDIA_BASE',    JPATH_WEB . '/' . $params->get($path, 'images'));
 define('COM_MEDIA_BASEURL', JUri::root() . $params->get($path, 'images'));
 
 $controller	= JControllerLegacy::getInstance('Media', array('base_path' => JPATH_COMPONENT_ADMINISTRATOR));
