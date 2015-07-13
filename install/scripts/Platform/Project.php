c73f27e715a7bc5b4fa41feeaa5704bdf4e54e41
Steven Rombauts <steven@timble.net>
2015-07-13 17:26:01 +0100

    #64 - Execute the site:create command

--- install/scripts/Platform/Project.php
+++ install/scripts/Platform/Project.php
@@ -1,11 +1,29 @@
 <?php
 namespace Platform;
 
+use Symfony\Component\Console\Input\ArrayInput;
+use Joomlatools\Console\Application;
+
 class Project
 {
     public static function install()
     {
-        
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            return;
+        }
 
+        $cwd  = getcwd();
+        $www  = dirname($cwd);
+        $site = basename($cwd);
+
+        $arguments = array(
+            'site:install',
+            'site'  => $site,
+            '--www' => $www,
+            '--interactive'
+        );
+
+        $application = new Application();
+        $application->run(new ArrayInput($arguments));
     }
 }
