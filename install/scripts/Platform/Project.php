fb3bd33ed91518ed91db5dff2020fd7b6078d89c
Steven Rombauts <steven@timble.net>
2015-07-13 18:35:25 +0100

    #64 - Add more verbosity to the installer

--- install/scripts/Platform/Project.php
+++ install/scripts/Platform/Project.php
@@ -2,6 +2,8 @@
 namespace Platform;
 
 use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\ConsoleOutput;
+
 use Joomlatools\Console\Application;
 
 class Project
@@ -23,7 +25,25 @@ class Project
             '--interactive' => true
         );
 
+        self::logo();
+
+        $output = new ConsoleOutput();
+        $output->writeln("<info>Welcome to the Joomla Platform installer</info>");
+        $output->writeln("Fill in the following details to configure your new application.");
+
         $application = new Application();
         $application->run(new ArrayInput($arguments));
+    }
+
+    public static function logo()
+    {
+        $output = new ConsoleOutput();
+
+        $output->writeln("                                                                ");
+        $output->writeln("    __               _        _____ _     _   ___               ");
+        $output->writeln(" __|  |___ ___ _____| |___   |  _  | |___| |_|  _|___ ___ _____ ");
+        $output->writeln("|  |  | . | . |     | | .'|  |   __| | .'|  _|  _| . |  _|     |");
+        $output->writeln("|_____|___|___|_|_|_|_|__,|  |__|  |_|__,|_| |_| |___|_| |_|_|_|");
+        $output->writeln("                                                                ");
     }
 }
