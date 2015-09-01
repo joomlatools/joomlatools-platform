28aa7a89174cf28382e43e94e95c38d6dcc5dbd7
Johan Janssens <johan@timble.net>
2015-09-01 23:17:48 +0200

    #237 - Update copyrights

--- config/configuration.php
+++ config/configuration.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Joomla Platform - http://developer.joomlatools.org/platform
+ *
+ * @copyright	Copyright (C) 2015 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		https://github.com/joomlatools/joomla-platform for the canonical source repository
+ */
+
 class JConfig
 {
     public function __construct()
