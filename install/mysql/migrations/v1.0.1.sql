3681770b9527e2c7141d429e2b03fee2d51096b0
Steven Rombauts <steven@timble.net>
2016-03-03 15:27:18 +0000

    #272 - Convert v1.0.1 SQL file to Phinx migration

--- install/mysql/migrations/v1.0.1.sql
+++
@@ -1,4 +0,0 @@
--- --------------------------------------------------------
--- Remove the debug plugin
--- Issue : https://github.com/joomlatools/joomlatools-platform/issues/261
-DELETE FROM `extensions` WHERE `extensions`.`name` = 'plg_system_debug'
