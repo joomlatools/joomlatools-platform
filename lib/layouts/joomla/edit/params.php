fca3d532bbf0a759295c0dafbef87408aee3a708
Allan Pilarca <allan@wizmedia.net>
2017-01-23 17:17:50 +0800

    #315: Apply joomla 3.6.5 changes in layouts

--- lib/layouts/joomla/edit/params.php
+++ lib/layouts/joomla/edit/params.php
@@ -3,21 +3,15 @@
  * @package     Joomla.Site
  * @subpackage  Layout
  *
- * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-defined('_JEXEC') or die;
+defined('JPATH_BASE') or die;
 
-$app = JFactory::getApplication();
-$form = $displayData->getForm();
-
-$fieldSets = $form->getFieldsets('params');
-
-// For BC with versions < 3.2 we need to render the attribs too
-$attribsFieldSet = $form->getFieldsets('attribs');
-
-$fieldSets = array_merge($fieldSets, $attribsFieldSet);
+$app       = JFactory::getApplication();
+$form      = $displayData->getForm();
+$fieldSets = $form->getFieldsets();
 
 if (empty($fieldSets))
 {
@@ -25,8 +19,8 @@ if (empty($fieldSets))
 }
 
 $ignoreFieldsets = $displayData->get('ignore_fieldsets') ?: array();
-$ignoreFields = $displayData->get('ignore_fields') ?: array();
-$extraFields = $displayData->get('extra_fields') ?: array();
+$ignoreFields    = $displayData->get('ignore_fields') ?: array();
+$extraFields     = $displayData->get('extra_fields') ?: array();
 
 if (!empty($displayData->hiddenFieldsets))
 {
@@ -46,24 +40,25 @@ if ($displayData->get('show_options', 1))
 	{
 		// Ensure any fieldsets we don't want to show are skipped (including repeating formfield fieldsets)
 		if (in_array($name, $ignoreFieldsets) || (!empty($configFieldsets) && in_array($name, $configFieldsets))
-				|| !empty($hiddenFieldsets) && in_array($name, $hiddenFieldsets)
-				|| (isset($fieldSet->repeat) && $fieldSet->repeat == true))
+			|| !empty($hiddenFieldsets) && in_array($name, $hiddenFieldsets)
+			|| (isset($fieldSet->repeat) && $fieldSet->repeat == true)
+		)
 		{
 			continue;
 		}
 
 		if (!empty($fieldSet->label))
 		{
-			$label = JText::_($fieldSet->label, true);
+			$label = JText::_($fieldSet->label);
 		}
 		else
 		{
 			$label = strtoupper('JGLOBAL_FIELDSET_' . $name);
-			if (JText::_($label, true) == $label)
+			if (JText::_($label) == $label)
 			{
 				$label = strtoupper($app->input->get('option') . '_' . $name . '_FIELDSET_LABEL');
 			}
-			$label = JText::_($label, true);
+			$label = JText::_($label);
 		}
 
 		echo JHtml::_('bootstrap.addTab', 'myTab', 'attrib-' . $name, $label);
@@ -81,7 +76,7 @@ if ($displayData->get('show_options', 1))
 }
 else
 {
-	$html = array();
+	$html   = array();
 	$html[] = '<div style="display:none;">';
 	foreach ($fieldSets as $name => $fieldSet)
 	{
@@ -94,7 +89,7 @@ else
 		{
 			foreach ($form->getFieldset($name) as $field)
 			{
-				echo $field->input;
+				$html[] = $field->input;
 			}
 		}
 	}
