338707da2a02433c726f5e21174537cc68242b5f
Allan Pilarca <allan@wizmedia.net>
2017-01-26 12:00:26 +0800

    #315: Apply joomla 3.6.5 changes in libraries

--- lib/libraries/cms/error/page.php
+++ lib/libraries/cms/error/page.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Libraries
  * @subpackage  Error
  *
- * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
  * @copyright   Copyright (C) 2015 Johan Janssens and Timble CVBA. (http://www.timble.net)
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
@@ -13,16 +13,14 @@ defined('JPATH_PLATFORM') or die;
 /**
  * Displays the custom error page when an uncaught exception occurs.
  *
- * @package     Joomla.Libraries
- * @subpackage  Error
- * @since       3.0
+ * @since  3.0
  */
 class JErrorPage
 {
 	/**
 	 * Render the error page based on an exception.
 	 *
-	 * @param   object  $error  An Exception or Throwable (PHP 7+) object for which to render the error page.
+	 * @param   Exception|Throwable  $error  An Exception or Throwable (PHP 7+) object for which to render the error page.
 	 *
 	 * @return  void
 	 *
@@ -38,8 +36,53 @@ class JErrorPage
 		{
 			try
 			{
-				$app      = JFactory::getApplication();
-				$document = JDocument::getInstance('error');
+				// Try to log the error, but don't let the logging cause a fatal error
+				try
+				{
+					JLog::add(
+						sprintf(
+							'Uncaught %1$s of type %2$s thrown. Stack trace: %3$s',
+							$expectedClass,
+							get_class($error),
+							$error->getTraceAsString()
+						),
+						JLog::CRITICAL,
+						'error'
+					);
+				}
+				catch (Throwable $e)
+				{
+					// Logging failed, don't make a stink about it though
+				}
+				catch (Exception $e)
+				{
+					// Logging failed, don't make a stink about it though
+				}
+
+				$app = JFactory::getApplication();
+
+				// If site is offline and it's a 404 error, just go to index (to see offline message, instead of 404)
+				if ($error->getCode() == '404' && JFactory::getConfig()->get('offline') == 1)
+				{
+					$app->redirect('index.php');
+				}
+
+				$attributes = array(
+					'charset'   => 'utf-8',
+					'lineend'   => 'unix',
+					'tab'       => "\t",
+					'language'  => 'en-GB',
+					'direction' => 'ltr',
+				);
+
+				// If there is a JLanguage instance in JFactory then let's pull the language and direction from its metadata
+				if (JFactory::$language)
+				{
+					$attributes['language']  = JFactory::getLanguage()->getTag();
+					$attributes['direction'] = JFactory::getLanguage()->isRtl() ? 'rtl' : 'ltr';
+				}
+
+				$document = JDocument::getInstance('error', $attributes);
 
                 $code = $error->getCode();
                 if(!isset(JHttpResponse::$status_messages[$code])) {
@@ -55,7 +98,7 @@ class JErrorPage
 				if (!$document || PHP_SAPI == 'cli')
 				{
 					// We're probably in an CLI environment
-					jexit($message);
+					jexit($error->getMessage());
 				}
 
 				// Get the current template from the application
@@ -64,17 +107,19 @@ class JErrorPage
 				// Push the error object into the document
 				$document->setError($error);
 
-				if (ob_get_contents()) {
+				if (ob_get_contents())
+				{
 					ob_end_clean();
 				}
 
-				$document->setTitle(JText::_('Error') . ': ' . $code);
+				$document->setTitle(JText::_('ERROR') . ': ' . $error->getCode());
+
 				$data = $document->render(
 					false,
 					array(
 						'template'  => $template,
 						'directory' => JPATH_THEMES,
-						'debug'     => JFactory::getConfig()->get('debug')
+						'debug'     => JDEBUG,
 					)
 				);
 
@@ -84,14 +129,21 @@ class JErrorPage
 				// If nothing was rendered, just use the message from the Exception
 				if (empty($data))
 				{
-					$data = $message;
+					$data = $error->getMessage();
 				}
 
 				$app->setBody($data);
 
 				echo $app->toString();
 
+				$app->close(0);
+
+				// This return is needed to ensure the test suite does not trigger the non-Exception handling below
 				return;
+			}
+			catch (Throwable $e)
+			{
+				// Pass the error down
 			}
 			catch (Exception $e)
 			{
@@ -109,7 +161,14 @@ class JErrorPage
 
 		if ($isException)
 		{
-			$message .= ': ' . $e->getMessage() . ': ' . $message;
+			$message .= ': ';
+
+			if (isset($e))
+			{
+				$message .= $e->getMessage() . ': ';
+			}
+
+			$message .= $error->getMessage();
 		}
 
 		echo $message;
