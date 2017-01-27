a018bd3fbc00996d792a71e86159f8a1c997c505
Allan Pilarca <allan@wizmedia.net>
2017-01-27 15:39:27 +0800

    #315: Apply joomla 3.6.5 changes in templates

--- web/administrator/templates/system/error.php
+++ web/administrator/templates/system/error.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Administrator
  * @subpackage  Template.system
  *
- * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
  * @copyright   Copyright (C) 2015 Johan Janssens and Timble CVBA. (http://www.timble.net)
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
@@ -14,12 +14,12 @@ defined('_JEXEC') or die;
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" >
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<title><?php echo $this->error->getCode(); ?> - <?php echo htmlspecialchars($this->message, ENT_QUOTES, 'UTF-8'); ?></title>
+	<title><?php echo $this->code; ?> - <?php echo htmlspecialchars($this->message, ENT_QUOTES, 'UTF-8'); ?></title>
 	<link rel="stylesheet" href="templates/system/css/error.css" type="text/css" />
 </head>
 <body>
 	<table width="550" align="center" class="outline">
-    <tr>
+	<tr>
 		<td align="center">
 			<h1>
 				<?php echo $this->code ?> - <?php echo JText::_('JERROR_AN_ERROR_HAS_OCCURRED') ?>
@@ -30,11 +30,26 @@ defined('_JEXEC') or die;
 		<td width="39%" align="center">
 			<p><?php echo htmlspecialchars($this->message, ENT_QUOTES, 'UTF-8'); ?></p>
 			<p><a href="index.php"><?php echo JText::_('JGLOBAL_TPL_CPANEL_LINK_TEXT') ?></a></p>
-			<p>
-				<?php if ($this->debug && ini_get('display_errors')) :
-					echo $this->renderBacktrace();
-				endif; ?>
-			</p>
+			<?php if ($this->debug && ini_get('display_errors')) : ?>
+				<div>
+					<?php echo $this->renderBacktrace(); ?>
+					<?php // Check if there are more Exceptions and render their data as well ?>
+					<?php if ($this->error->getPrevious()) : ?>
+						<?php $loop = true; ?>
+						<?php // Reference $this->_error here and in the loop as setError() assigns errors to this property and we need this for the backtrace to work correctly ?>
+						<?php // Make the first assignment to setError() outside the loop so the loop does not skip Exceptions ?>
+						<?php $this->setError($this->_error->getPrevious()); ?>
+						<?php while ($loop === true) : ?>
+							<p><strong><?php echo JText::_('JERROR_LAYOUT_PREVIOUS_ERROR'); ?></strong></p>
+							<p><?php echo htmlspecialchars($this->_error->getMessage(), ENT_QUOTES, 'UTF-8'); ?></p>
+							<?php echo $this->renderBacktrace(); ?>
+							<?php $loop = $this->setError($this->_error->getPrevious()); ?>
+						<?php endwhile; ?>
+						<?php // Reset the main error object to the base error ?>
+						<?php $this->setError($this->error); ?>
+					<?php endif; ?>
+				</div>
+			<?php endif; ?>
 		</td>
 	</tr>
 	</table>
